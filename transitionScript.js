33e5312f338029a42db11c324b70245c4ed5c823
MT-Cobbler <mtschoeman123@gmail.com>
2020-11-03 12:08:02 +0200

    added transitions

--- transitionScript.js
+++ transitionScript.js
@@ -1,1 +1,9 @@
 const swup = new Swup();
+
+function fadeIn() {
+    var body = document.querySelector('body');
+    body.style.transition = "margin-left 0.3s linear 0s";
+    body.style.marginLeft = 0;
+    body.style.opacity = 1;
+    console.log("hello");
+}
